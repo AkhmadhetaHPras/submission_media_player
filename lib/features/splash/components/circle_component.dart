1d3c6a9bb05f18239fa1da3d72e37dac55e2c5e1
Akhmadheta Hafid Prasetyawan <77179142+AkhmadhetaHPras@users.noreply.github.com>
2024-07-18 16:42:46 +0700

    Update circle_component.dart

--- lib/features/splash/components/circle_component.dart
+++ lib/features/splash/components/circle_component.dart
@@ -19,7 +19,7 @@ class CircleComponent extends StatelessWidget {
         child: child ??
             Container(
               decoration: const BoxDecoration(
-                shape: BoxShape.ractangle,
+                shape: BoxShape.rectangle,
                 // color: color,
               ),
             ),
